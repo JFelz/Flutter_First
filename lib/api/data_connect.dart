60ad3432c79bc7da250623cae1bf6b31073b9c37
Jovanni Feliz <feliz.jovanni@gmail.com>
2024-09-01 23:25:51 -0500

    "installed updated APK -- Found issues with build previously -- build is successful on Android Studio "

--- lib/api/data_connect.dart
+++ lib/api/data_connect.dart
@@ -1,34 +1,34 @@
-import 'package:http/http.dart' as http;
+// import 'package:http/http.dart' as http;
 
 
-final url = Uri.https('https://tabletalkz-5c9e1-default-rtdb.firebaseio.com/');
+// final url = Uri.https('https://tabletalkz-5c9e1-default-rtdb.firebaseio.com/');
 
-final response = http.get(url);
-
-
-// Future<http.Response> fetchQuestion() {
+// final response = http.get(url);
 
 
-//   final url = Uri.https('https://tabletalkz-5c9e1-default-rtdb.firebaseio.com/');
+// // Future<http.Response> fetchQuestion() {
 
-// final response = await http.get(url);
 
-// print('Response body: ${response.body}');
+// //   final url = Uri.https('https://tabletalkz-5c9e1-default-rtdb.firebaseio.com/');
+
+// // final response = await http.get(url);
+
+// // print('Response body: ${response.body}');
   
-// }
+// // }
 
-// class Question {
-//   final String description;
+// // class Question {
+// //   final String description;
 
-// const Question({required this.description});
+// // const Question({required this.description});
 
-// factory Question.fromJson(Map<String, dynamic> json){
-// return switch (json) {
-//   {
-//   'description': String description,
-//   } => Question(description: description),
-//   _=> throw const FormatException('Failed to load question')
-// };
+// // factory Question.fromJson(Map<String, dynamic> json){
+// // return switch (json) {
+// //   {
+// //   'description': String description,
+// //   } => Question(description: description),
+// //   _=> throw const FormatException('Failed to load question')
+// // };
 
-// }
-// }
+// // }
+// // }
